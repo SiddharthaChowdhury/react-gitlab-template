21b8bf0f1ed25700f4ff38af20aea72f4d0a2ab5
sid <siddhartha@applike.info>
2019-06-25 15:30:36 +0200

    Style adjustments

--- app/src/component/layout/dashboard/fixedLeftNav/expanded/FixedLeftNav.tsx
+++ app/src/component/layout/dashboard/fixedLeftNav/expanded/FixedLeftNav.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDoubleLeft } from '@fortawesome/free-solid-svg-icons'
 import './fixedLeftNav.scss';
 
 export class FixedLeftNav extends React.PureComponent {
     public render() {
         return (
             <div className="fixedLeftNav">
-                <div className="fixedLeftNav-toggleButton"></div>
+                <div className="fixedLeftNav-toggleButton">
+                    <FontAwesomeIcon icon={faAngleDoubleLeft} className="collapse-icon"/>
+                    <span>Collapse sidebar</span>
+                </div>
             </div>
         )
     }
