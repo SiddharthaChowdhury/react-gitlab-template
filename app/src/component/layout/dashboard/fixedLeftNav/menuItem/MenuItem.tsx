abd3ff16940d61e52130de14c6458d79ea848a49
sid <siddhartha@applike.info>
2019-06-26 13:48:11 +0200

    Popup menu added

--- app/src/component/layout/dashboard/fixedLeftNav/menuItem/MenuItem.tsx
+++ app/src/component/layout/dashboard/fixedLeftNav/menuItem/MenuItem.tsx
@@ -3,29 +3,13 @@ import './menuItem.scss'
 import { IMenuItem } from './IMenuItem';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IMenuItemSubMenuList } from '../../../../../genericTypes/IMenuItemSubMenuList';
+import Popup from "reactjs-popup";
 
 interface IMenuItemProps extends IMenuItem{}
 
 export class MenuItem extends React.PureComponent<IMenuItemProps> {
     public render () {
-        const {
-            text,
-            icon: Icon,
-            // trigger,
-            // subMenuList,
-            isEnabled
-        } = this.props
-
-        return (
-            <div className={isEnabled ? "menuItem-enabled" : "menuItem"}>
-                <div className={isEnabled ? "menuItem-element-enabled menuItem-element" : "menuItem-element"} >
-                    <FontAwesomeIcon icon={Icon} className="menuItem-element-icon"/>
-                    <span>{text}</span>
-                    {this.getBubbleNumber()}
-                </div>
-                { isEnabled && this.getSubMenuList()}
-            </div>
-        );
+        return this.getSubMenuPopupWrappedMenuButton()
     }
 
     private getBubbleNumber = () => {
@@ -55,12 +39,57 @@ export class MenuItem extends React.PureComponent<IMenuItemProps> {
             <div>
                 {subMenuList.map((item: IMenuItemSubMenuList, index: number) => {
                     return (
-                        <div className={item.isEnabled? "menuItem-subElement menuItem-subElement-active" : "menuItem-subElement"}>
+                        <div className={item.isEnabled? "menuItem-subElement menuItem-subElement-active" : "menuItem-subElement"} key={index}>
                             <div>{item.text}</div>
                         </div>
                     )
                 })}
             </div>
         )
+    }
+
+    private getSubMenuPopupWrappedMenuButton = () => {
+        const { isEnabled, subMenuList } = this.props;
+        const menuButton = this.getMenuButton();
+
+        if(!isEnabled && subMenuList) {
+            return (
+                <Popup
+                    trigger={menuButton}
+                    position="right top"
+                    on="hover"
+                    closeOnDocumentClick
+                    mouseLeaveDelay={100}
+                    mouseEnterDelay={0}
+                    contentStyle={{ padding: "0px", border: "none" }}
+                    arrow={false}
+                    >
+                    <div className="popupMenu">
+                        {this.getSubMenuList()}
+                    </div>
+                </Popup>
+            );
+        }
+
+        return menuButton
+    }
+
+    private getMenuButton = () => {
+        const {
+            text,
+            icon: Icon,
+            isEnabled,
+        } = this.props
+
+        return (
+            <div className={isEnabled ? "menuItem-enabled" : "menuItem"}>
+                <div className={isEnabled ? "menuItem-element-enabled menuItem-element" : "menuItem-element"} >
+                    <FontAwesomeIcon icon={Icon} className="menuItem-element-icon"/>
+                    <span>{text}</span>
+                    {this.getBubbleNumber()}
+                </div>
+                { isEnabled && this.getSubMenuList()}
+            </div>
+        );
     }
 }
