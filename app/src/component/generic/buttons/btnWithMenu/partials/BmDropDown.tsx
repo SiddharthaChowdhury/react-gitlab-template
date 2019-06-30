cae25c248885bf8fec911e1ae46bcb80ec1469f4
Siddhartha <siddharthac6@gmail.com>
2019-06-30 23:39:06 +0530

    Added select buttons

--- app/src/component/generic/buttons/btnWithMenu/partials/BmDropDown.tsx
+++ app/src/component/generic/buttons/btnWithMenu/partials/BmDropDown.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Menu from "@material-ui/core/Menu";
 import {faCaretDown} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {IdBtnColor} from "../../IdBtnColor";
 import {utilBtn} from "../../utilBtn";
+import {StyledMenu} from "../../StypedMenu";
 
 interface IBmDropDownProps {
     color?: IdBtnColor;
@@ -22,7 +22,6 @@ export const BmDropDown: React.FC<IBmDropDownProps> = (props) => {
 
   return (
     <div style={{display: 'flex', alignItems: 'center'}}>
-      {/* TODO: This needs to be styled as drop down icon button */}
       <button className={`partial__menuBtn partial__menuBtnDownBtn ${utilBtn.getColorClass(props.color)}`} onClick={handleClick} style={{borderRadius: '0px 3px 3px 0px'}}>
           <FontAwesomeIcon icon={faCaretDown} className="menuItem-element-icon"/>
       </button>
@@ -38,19 +37,3 @@ export const BmDropDown: React.FC<IBmDropDownProps> = (props) => {
     </div>
   );
 }
-
-const StyledMenu = (props: any) => (
-    <Menu
-      elevation={0}
-      getContentAnchorEl={null}
-      anchorOrigin={{
-        vertical: "bottom",
-        horizontal: "center"
-      }}
-      transformOrigin={{
-        vertical: "top",
-        horizontal: "center"
-      }}
-      {...props}
-    />
-  );
--- app/src/component/layout/dashboard/content/Content.tsx
+++ app/src/component/layout/dashboard/content/Content.tsx
@@ -8,6 +8,7 @@ import {Heading} from '../../../generic/typography/heading/Heading';
 import {Btn} from '../../../generic/buttons/Btn';
 import {IdBtnColor} from '../../../generic/buttons/IdBtnColor';
 import {BtnMenu} from '../../../generic/buttons/btnWithMenu/BtnMenu';
+import {BtnSelect} from "../../../generic/buttons/btnSelect/BtnSelect";
 
 interface IContentOwnState {
     navStatus: IdNavStatus
@@ -33,41 +34,38 @@ export class Content extends React.PureComponent {
                         <Heading> This is "Heading" from typography</Heading>
                     </Segment>
                     <Segment>
-                        <Btn text={'Default Btn'} className='container-btn-elements'/>
                         <Btn text={'Disable Btn'} solid={true} className='container-btn-elements'/>
-
+                        <Btn text={'Default Btn'} className='container-btn-elements'/>
                         <Btn text={'Green Btn'} color={IdBtnColor.GREEN} className='container-btn-elements' />
-                        <Btn text={'Green Btn'} color={IdBtnColor.GREEN} solid={true} className='container-btn-elements' />
-
                         <Btn text={'Orange Btn'} color={IdBtnColor.ORANGE} className='container-btn-elements'/>
-                        <Btn text={'Orange Btn'} color={IdBtnColor.ORANGE} solid={true} className='container-btn-elements' />
-
                         <Btn text={'Blue Btn'} color={IdBtnColor.BLUE} className='container-btn-elements'/>
-                        <Btn text={'Blue Btn'} color={IdBtnColor.BLUE} solid={true} className='container-btn-elements' />
-
                         <Btn text={'Red Btn'} color={IdBtnColor.RED} className='container-btn-elements' />
+
+                        <Btn text={'Green Btn'} color={IdBtnColor.GREEN} solid={true} className='container-btn-elements' />
+                        <Btn text={'Orange Btn'} color={IdBtnColor.ORANGE} solid={true} className='container-btn-elements' />
+                        <Btn text={'Blue Btn'} color={IdBtnColor.BLUE} solid={true} className='container-btn-elements' />
                         <Btn text={'Red Btn'} color={IdBtnColor.RED} solid={true} className='container-btn-elements' />
                     </Segment>
                     <Segment>
-                        <BtnMenu text="button BtnMenu" menu={
-                            <h3>Menu Content in dropdown</h3>
-                        } className='container-btn-elements'/>
+                        <BtnMenu text="button BtnMenu" menu={getDummyMenu()} className='container-btn-elements'/>
+                        <BtnMenu text="button BtnMenu" color={IdBtnColor.BLUE} menu={getDummyMenu()} className='container-btn-elements' />
+                        <BtnMenu text="button BtnMenu" color={IdBtnColor.RED} menu={getDummyMenu()} className='container-btn-elements' />
+                        <BtnMenu text="button BtnMenu" color={IdBtnColor.GREEN} menu={getDummyMenu()} className='container-btn-elements' />
+                        <BtnMenu text="button BtnMenu" color={IdBtnColor.ORANGE} menu={getDummyMenu()} className='container-btn-elements' />
+                    </Segment>
+                    <Segment>
+                        <BtnSelect text={'BtnSelect'} options={getDummySelectItems()} className='container-btn-elements' />
+                        <BtnSelect text={'BtnSelect'} options={getDummySelectItems()} color={IdBtnColor.GREEN} className='container-btn-elements' />
+                        <BtnSelect text={'BtnSelect'} options={getDummySelectItems()} color={IdBtnColor.BLUE} className='container-btn-elements'/>
+                        <BtnSelect text={'BtnSelect'} options={getDummySelectItems()} color={IdBtnColor.ORANGE} className='container-btn-elements' />
+                        <BtnSelect text={'BtnSelect'} options={getDummySelectItems()} color={IdBtnColor.RED} className='container-btn-elements' />
 
-                        <BtnMenu text="button BtnMenu" color={IdBtnColor.BLUE} menu={
-                            <h3>Menu Content in dropdown</h3>
-                        } className='container-btn-elements' />
-
-                        <BtnMenu text="button BtnMenu" color={IdBtnColor.RED} menu={
-                            <h3>Menu Content in dropdown</h3>
-                        } className='container-btn-elements' />
-
-                        <BtnMenu text="button BtnMenu" color={IdBtnColor.GREEN} menu={
-                            <h3>Menu Content in dropdown</h3>
-                        } className='container-btn-elements' />
-
-                        <BtnMenu text="button BtnMenu" color={IdBtnColor.ORANGE} menu={
-                            <h3>Menu Content in dropdown</h3>
-                        } className='container-btn-elements' />
+                        {/*------------------------------*/}
+                        <BtnSelect text={'BtnSelect'} options={getDummySelectItems()} className='container-btn-elements' solid={true} />
+                        <BtnSelect text={'BtnSelect'} options={getDummySelectItems()} color={IdBtnColor.GREEN} className='container-btn-elements' solid={true} />
+                        <BtnSelect text={'BtnSelect'} options={getDummySelectItems()} color={IdBtnColor.BLUE} className='container-btn-elements' solid={true}/>
+                        <BtnSelect text={'BtnSelect'} options={getDummySelectItems()} color={IdBtnColor.ORANGE} className='container-btn-elements' solid={true} />
+                        <BtnSelect text={'BtnSelect'} options={getDummySelectItems()} color={IdBtnColor.RED} className='container-btn-elements' solid={true} />
                     </Segment>
                 </Container>
             </div>
@@ -82,4 +80,12 @@ export const Segment = ({children}: any) => {
             {children}
         </div>
     )
-}
+};
+
+const getDummyMenu = () => (<h3>Menu Content in dropdown</h3>);
+
+const getDummySelectItems = () => ([
+    <a href="/" style={{width: '100%'}}> Item 1 </a>,
+    <a href="/" style={{width: '100%'}}> Item 2 </a>,
+    <a href="/" style={{width: '100%'}}> Item 3 </a>,
+]);
